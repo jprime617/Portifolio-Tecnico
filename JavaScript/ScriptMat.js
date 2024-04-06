36f5950cbc126dc2dc89f8b41f13bb6ca6e9061f
joão pedro silva <130461387+jprime617@users.noreply.github.com>
2024-04-06 15:16:54 -0300

    commir pagbuajfw4rnb

---
+++ JavaScript/ScriptMat.js
@@ -0,0 +1,401 @@
+function Ano(){
+    var Anos = document.getElementById("Anos")
+    var option = Anos.value
+
+    var Ano = ""
+    var Atv01_Titulo = ""
+    var Atv01_Paragrafo = ""
+    var Atv01_Img = ""
+    var Atv01_A = ""
+
+    var Atv02_Titulo = ""
+    var Atv02_Paragrafo = ""
+    var Atv02_Img = ""
+    var Atv02_A = ""
+
+    var Atv03_Titulo = ""
+    var Atv03_Paragrafo = ""
+    var Atv03_Img = ""
+    var Atv03_A = ""
+
+    var Atv04_Titulo = ""
+    var Atv04_Paragrafo = ""
+    var Atv04_Img = ""
+    var Atv04_A = ""
+
+    var Atv05_Titulo = ""
+    var Atv05_Paragrafo = ""
+    var Atv05_Img = ""
+    var Atv05_A = ""
+
+    var Atv06_Titulo = ""
+    var Atv06_Paragrafo = ""
+    var Atv06_Img = ""
+    var Atv06_A = ""
+
+    var Atv07_Titulo = ""
+    var Atv07_Paragrafo = "."
+    var Atv07_Img = ""
+    var Atv07_A = ""
+
+    var Atv08_Titulo = ""
+    var Atv08_Paragrafo = ""
+    var Atv08_Img = ""
+    var Atv08_A = ""
+
+    var Atv09_Titulo = ""
+    var Atv09_Paragrafo = ""
+    var Atv09_Img = ""
+    var Atv09_A = ""
+
+    var Atv10_Titulo = ""
+    var Atv10_Paragrafo = ""
+    var Atv10_Img = ""
+    var Atv10_A = ""
+
+    var Atv11_Titulo = ""
+    var Atv11_Paragrafo = ""
+    var Atv11_Img = ""
+    var Atv11_A = ""
+
+    var Atv12_Titulo = ""
+    var Atv12_Paragrafo = ""
+    var Atv12_Img = ""
+    var Atv12_A = ""
+
+
+    //-----------------------------------------------------------------------------------------------------------------------------
+
+
+    if(option == "opcao1"){
+
+    var Ano = "1º Ano"
+    var Atv01_Titulo = "Triângulos: razões trigonométricas, lei dos senos e cossenos."
+    var Atv01_Paragrafo = "Nessa atividade eu fiz um mapa conceitual sobre um capitulo da geekie one."
+    var Atv01_Img = "../assets/Mat/trignometria.webp"
+    var Atv01_A = "https://docs.google.com/document/d/1PXD-ZBnWOph8oicQ852t4lbJfIwvB6UEubr4K8pDIvo/edit?usp=sharing"
+
+
+    var Atv02_Titulo = "Atividade razão e proporção."
+    var Atv02_Paragrafo = "Nessa atividade tivemos que nos medir e medir um quarto e fazer uma escala proporcional."
+    var Atv02_Img = "../assets/Mat/expressao-matematica-de-uma-proporcao-que-e-uma-igualdade-entre-razoes.jpg"
+    var Atv02_A = "https://docs.google.com/document/d/1UxGyJSBMmUmx4GdxXmBBgLOCnr51oRjviLW93ieqIeU/edit?usp=sharing"
+
+
+    var Atv03_Titulo = "Resumo trigonometria."
+    var Atv03_Paragrafo = "Um resumo feito em folha."
+    var Atv03_Img = "../assets/Mat/trigonometria.webp"
+    var Atv03_A = "https://docs.google.com/document/d/1KkdfSb2CSZ67eIRFHNJzHOg7AYbz2t_s6cw7sfg6PCE/edit?usp=sharing"
+
+
+    var Atv04_Titulo = "Razão."
+    var Atv04_Paragrafo = "Um resumo de razão feito no caderno."
+    var Atv04_Img = "../assets/Mat/1-razao.webp"
+    var Atv04_A = "https://docs.google.com/document/d/1mRotVGa8gY9hdkuG8g93s9iJVlBUPGC1upQLgWL1tIo/edit?usp=sharing"
+
+
+    var Atv05_Titulo = "Função no geogebra."
+    var Atv05_Paragrafo = "nessa atividade traçamos linhas no geogebra."
+    var Atv05_Img = "../assets/Mat/geogebra.png"
+    var Atv05_A = "https://docs.google.com/document/d/14XnYSw6SR3o6r-CVaOkN9h-TVClYxufGazDRUR6dibM/edit?usp=sharing"
+
+
+    var Atv06_Titulo = "História dos Conjuntos Numéricos."
+    var Atv06_Paragrafo = "nessa atividade fizemos a historia da nossa apresentaçao de matematica."
+    var Atv06_Img = "../assets/Mat/conjuntos.webp"
+    var Atv06_A = "https://docs.google.com/document/d/12ZCuj3pUQ0eUNYsDTaFMtMLfdY58Qq2yt9RiAGEm1hY/edit?usp=sharing"
+
+
+    var Atv07_Titulo = "Função."
+    var Atv07_Paragrafo = "Para que serve a função."
+    var Atv07_Img = "../assets/Mat/funcao.webp"
+    var Atv07_A = "https://docs.google.com/document/d/1fYYC-7qtaEny7Go9zsAxy2KBt20hX936uzejSZU4Qy0/edit?usp=sharing"
+
+
+    var Atv08_Titulo = "Conjunto."
+    var Atv08_Paragrafo = "Conjuntos simples."
+    var Atv08_Img = "../assets/Mat/conjunto.webp"
+    var Atv08_A = "https://docs.google.com/document/d/1QjRtRBXHLxp7WCG7ljApnJ4wtmj2sjod9jjMsCvi-ro/edit?usp=sharing"
+
+
+    var Atv09_Titulo = "Exercícios sobre PA."
+    var Atv09_Paragrafo = "atividade sobre PA."
+    var Atv09_Img = "../assets/Mat/PA.webp"
+    var Atv09_A = "https://docs.google.com/document/d/1Q5t1gZ4I2KiwWqfHPQhhhYgspnFYXXROG7KTHrDd33E/edit?usp=sharing"
+
+
+    var Atv10_Titulo = "Mapa mental."
+    var Atv10_Paragrafo = "Realizar um resumo no caderno sobre Sequência e Lógica, dois exercícios resolvidos Um mapa mental sobre Progressão Aritmética e no caderno dois exercícios resolvidos."
+    var Atv10_Img = "../assets/Mat/pi.webp"
+    var Atv10_A = "https://docs.google.com/document/d/1Zt-OKLV4CxKhe29m3gbn9B5BkO4U8kLPO1rBU3UnIho/edit?usp=sharing"
+
+
+    var Atv11_Titulo = "Mundo senai."
+    var Atv11_Paragrafo = "4rs animal."
+    var Atv11_Img = "../assets/Mat/cachorro.jpeg"
+    var Atv11_A = "https://www.canva.com/design/DAFPh6aVm_w/j4K8WL9odOvyS2t_sdpV5w/view?utm_content=DAFPh6aVm_w&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu"
+
+
+    var Atv12_Titulo = "Einstein e seu neto."
+    var Atv12_Paragrafo = "uma foto de mim e meu avô usando usando uma fantasia improvisada."
+    var Atv12_Img = "../assets/Mat/einstein.webp"
+    var Atv12_A = "https://docs.google.com/document/d/10sOk9XhWxXXCcjGuF5LwqzA4ZAru7Sl57DHLmn48Yjc/edit?usp=sharing"
+
+
+    //Atv01
+    document.getElementById("Ano").innerHTML = Ano
+    document.getElementById("Atv01_Titulo").innerHTML = Atv01_Titulo
+    document.getElementById("Atv01_Paragrafo").innerHTML = Atv01_Paragrafo
+    document.getElementById("Atv01_Img").src = Atv01_Img
+    document.getElementById("Atv01_A").href = Atv01_A
+
+    //Atv02
+    document.getElementById("Ano").innerHTML = Ano
+    document.getElementById("Atv02_Titulo").innerHTML = Atv02_Titulo
+    document.getElementById("Atv02_Paragrafo").innerHTML = Atv02_Paragrafo
+    document.getElementById("Atv02_Img").src = Atv02_Img
+    document.getElementById("Atv02_A").href = Atv02_A
+
+    //Atv03
+    document.getElementById("Ano").innerHTML = Ano
+    document.getElementById("Atv03_Titulo").innerHTML = Atv03_Titulo
+    document.getElementById("Atv03_Paragrafo").innerHTML = Atv03_Paragrafo
+    document.getElementById("Atv03_Img").src = Atv03_Img
+    document.getElementById("Atv03_A").href = Atv03_A
+
+    //Atv04
+    document.getElementById("Ano").innerHTML = Ano
+    document.getElementById("Atv04_Titulo").innerHTML = Atv04_Titulo
+    document.getElementById("Atv04_Paragrafo").innerHTML = Atv04_Paragrafo
+    document.getElementById("Atv04_Img").src = Atv04_Img
+    document.getElementById("Atv04_A").href = Atv04_A
+
+    //Atv05
+    document.getElementById("Ano").innerHTML = Ano
+    document.getElementById("Atv05_Titulo").innerHTML = Atv05_Titulo
+    document.getElementById("Atv05_Paragrafo").innerHTML = Atv05_Paragrafo
+    document.getElementById("Atv05_Img").src = Atv05_Img
+    document.getElementById("Atv05_A").href = Atv05_A
+
+    //Atv06
+    document.getElementById("Ano").innerHTML = Ano
+    document.getElementById("Atv06_Titulo").innerHTML = Atv06_Titulo
+    document.getElementById("Atv06_Paragrafo").innerHTML = Atv06_Paragrafo
+    document.getElementById("Atv06_Img").src = Atv06_Img
+    document.getElementById("Atv06_A").href = Atv06_A
+
+    //Atv07
+    document.getElementById("Ano").innerHTML = Ano
+    document.getElementById("Atv07_Titulo").innerHTML = Atv07_Titulo
+    document.getElementById("Atv07_Paragrafo").innerHTML = Atv07_Paragrafo
+    document.getElementById("Atv07_Img").src = Atv07_Img
+    document.getElementById("Atv07_A").href = Atv07_A
+
+    //Atv08
+    document.getElementById("Ano").innerHTML = Ano
+    document.getElementById("Atv08_Titulo").innerHTML = Atv08_Titulo
+    document.getElementById("Atv08_Paragrafo").innerHTML = Atv08_Paragrafo
+    document.getElementById("Atv08_Img").src = Atv08_Img
+    document.getElementById("Atv08_A").href = Atv08_A
+
+    //Atv09
+    document.getElementById("Ano").innerHTML = Ano
+    document.getElementById("Atv09_Titulo").innerHTML = Atv09_Titulo
+    document.getElementById("Atv09_Paragrafo").innerHTML = Atv09_Paragrafo
+    document.getElementById("Atv09_Img").src = Atv09_Img
+    document.getElementById("Atv09_A").href = Atv09_A
+
+    //Atv10
+    document.getElementById("Ano").innerHTML = Ano
+    document.getElementById("Atv10_Titulo").innerHTML = Atv10_Titulo
+    document.getElementById("Atv10_Paragrafo").innerHTML = Atv10_Paragrafo
+    document.getElementById("Atv10_Img").src = Atv10_Img
+    document.getElementById("Atv10_A").href = Atv10_A
+
+    //Atv11
+    document.getElementById("Ano").innerHTML = Ano
+    document.getElementById("Atv11_Titulo").innerHTML = Atv11_Titulo
+    document.getElementById("Atv11_Paragrafo").innerHTML = Atv11_Paragrafo
+    document.getElementById("Atv11_Img").src = Atv11_Img
+    document.getElementById("Atv11_A").href = Atv11_A
+
+    //Atv12
+    document.getElementById("Ano").innerHTML = Ano
+    document.getElementById("Atv12_Titulo").innerHTML = Atv12_Titulo
+    document.getElementById("Atv12_Paragrafo").innerHTML = Atv12_Paragrafo
+    document.getElementById("Atv12_Img").src = Atv12_Img
+    document.getElementById("Atv12_A").href = Atv12_A
+
+    }else if(option == opcao2){
+
+        var Ano = "2º Ano"
+        var Atv01_Titulo = "ewbhgviujs."
+        var Atv01_Paragrafo = "Nessa atividade eu fiz um mapa conceitual sobre um capitulo da geekie one."
+        var Atv01_Img = "../assets/Mat/trignometria.webp"
+        var Atv01_A = "https://docs.google.com/document/d/1PXD-ZBnWOph8oicQ852t4lbJfIwvB6UEubr4K8pDIvo/edit?usp=sharing"
+    
+    
+        var Atv02_Titulo = "Atividade razão e proporção."
+        var Atv02_Paragrafo = "Nessa atividade tivemos que nos medir e medir um quarto e fazer uma escala proporcional."
+        var Atv02_Img = "../assets/Mat/expressao-matematica-de-uma-proporcao-que-e-uma-igualdade-entre-razoes.jpg"
+        var Atv02_A = "https://docs.google.com/document/d/1UxGyJSBMmUmx4GdxXmBBgLOCnr51oRjviLW93ieqIeU/edit?usp=sharing"
+    
+    
+        var Atv03_Titulo = "Resumo trigonometria."
+        var Atv03_Paragrafo = "Um resumo feito em folha."
+        var Atv03_Img = "../assets/Mat/trigonometria.webp"
+        var Atv03_A = "https://docs.google.com/document/d/1KkdfSb2CSZ67eIRFHNJzHOg7AYbz2t_s6cw7sfg6PCE/edit?usp=sharing"
+    
+    
+        var Atv04_Titulo = "Razão."
+        var Atv04_Paragrafo = "Um resumo de razão feito no caderno."
+        var Atv04_Img = "../assets/Mat/1-razao.webp"
+        var Atv04_A = "https://docs.google.com/document/d/1mRotVGa8gY9hdkuG8g93s9iJVlBUPGC1upQLgWL1tIo/edit?usp=sharing"
+    
+    
+        var Atv05_Titulo = "Função no geogebra."
+        var Atv05_Paragrafo = "nessa atividade traçamos linhas no geogebra."
+        var Atv05_Img = "../assets/Mat/geogebra.png"
+        var Atv05_A = "https://docs.google.com/document/d/14XnYSw6SR3o6r-CVaOkN9h-TVClYxufGazDRUR6dibM/edit?usp=sharing"
+    
+    
+        var Atv06_Titulo = "História dos Conjuntos Numéricos."
+        var Atv06_Paragrafo = "nessa atividade fizemos a historia da nossa apresentaçao de matematica."
+        var Atv06_Img = "../assets/Mat/conjuntos.webp"
+        var Atv06_A = "https://docs.google.com/document/d/12ZCuj3pUQ0eUNYsDTaFMtMLfdY58Qq2yt9RiAGEm1hY/edit?usp=sharing"
+    
+    
+        var Atv07_Titulo = "Função."
+        var Atv07_Paragrafo = "Para que serve a função."
+        var Atv07_Img = "../assets/Mat/funcao.webp"
+        var Atv07_A = "https://docs.google.com/document/d/1fYYC-7qtaEny7Go9zsAxy2KBt20hX936uzejSZU4Qy0/edit?usp=sharing"
+    
+    
+        var Atv08_Titulo = "Conjunto."
+        var Atv08_Paragrafo = "Conjuntos simples."
+        var Atv08_Img = "../assets/Mat/conjunto.webp"
+        var Atv08_A = "https://docs.google.com/document/d/1QjRtRBXHLxp7WCG7ljApnJ4wtmj2sjod9jjMsCvi-ro/edit?usp=sharing"
+    
+    
+        var Atv09_Titulo = "Exercícios sobre PA."
+        var Atv09_Paragrafo = "atividade sobre PA."
+        var Atv09_Img = "../assets/Mat/PA.webp"
+        var Atv09_A = "https://docs.google.com/document/d/1Q5t1gZ4I2KiwWqfHPQhhhYgspnFYXXROG7KTHrDd33E/edit?usp=sharing"
+    
+    
+        var Atv10_Titulo = "Mapa mental."
+        var Atv10_Paragrafo = "Realizar um resumo no caderno sobre Sequência e Lógica, dois exercícios resolvidos Um mapa mental sobre Progressão Aritmética e no caderno dois exercícios resolvidos."
+        var Atv10_Img = "../assets/Mat/pi.webp"
+        var Atv10_A = "https://docs.google.com/document/d/1Zt-OKLV4CxKhe29m3gbn9B5BkO4U8kLPO1rBU3UnIho/edit?usp=sharing"
+    
+    
+        var Atv11_Titulo = "Mundo senai."
+        var Atv11_Paragrafo = "4rs animal."
+        var Atv11_Img = "../assets/Mat/cachorro.jpeg"
+        var Atv11_A = "https://www.canva.com/design/DAFPh6aVm_w/j4K8WL9odOvyS2t_sdpV5w/view?utm_content=DAFPh6aVm_w&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu"
+    
+    
+        var Atv12_Titulo = "Einstein e seu neto."
+        var Atv12_Paragrafo = "uma foto de mim e meu avô usando usando uma fantasia improvisada."
+        var Atv12_Img = "../assets/Mat/einstein.webp"
+        var Atv12_A = "https://docs.google.com/document/d/10sOk9XhWxXXCcjGuF5LwqzA4ZAru7Sl57DHLmn48Yjc/edit?usp=sharing"
+    
+    
+        //Atv01
+        document.getElementById("Ano").innerHTML = Ano
+        document.getElementById("Atv01_Titulo").innerHTML = Atv01_Titulo
+        document.getElementById("Atv01_Paragrafo").innerHTML = Atv01_Paragrafo
+        document.getElementById("Atv01_Img").src = Atv01_Img
+        document.getElementById("Atv01_A").href = Atv01_A
+    
+        //Atv02
+        document.getElementById("Ano").innerHTML = Ano
+        document.getElementById("Atv02_Titulo").innerHTML = Atv02_Titulo
+        document.getElementById("Atv02_Paragrafo").innerHTML = Atv02_Paragrafo
+        document.getElementById("Atv02_Img").src = Atv02_Img
+        document.getElementById("Atv02_A").href = Atv02_A
+    
+        //Atv03
+        document.getElementById("Ano").innerHTML = Ano
+        document.getElementById("Atv03_Titulo").innerHTML = Atv03_Titulo
+        document.getElementById("Atv03_Paragrafo").innerHTML = Atv03_Paragrafo
+        document.getElementById("Atv03_Img").src = Atv03_Img
+        document.getElementById("Atv03_A").href = Atv03_A
+    
+        //Atv04
+        document.getElementById("Ano").innerHTML = Ano
+        document.getElementById("Atv04_Titulo").innerHTML = Atv04_Titulo
+        document.getElementById("Atv04_Paragrafo").innerHTML = Atv04_Paragrafo
+        document.getElementById("Atv04_Img").src = Atv04_Img
+        document.getElementById("Atv04_A").href = Atv04_A
+    
+        //Atv05
+        document.getElementById("Ano").innerHTML = Ano
+        document.getElementById("Atv05_Titulo").innerHTML = Atv05_Titulo
+        document.getElementById("Atv05_Paragrafo").innerHTML = Atv05_Paragrafo
+        document.getElementById("Atv05_Img").src = Atv05_Img
+        document.getElementById("Atv05_A").href = Atv05_A
+    
+        //Atv06
+        document.getElementById("Ano").innerHTML = Ano
+        document.getElementById("Atv06_Titulo").innerHTML = Atv06_Titulo
+        document.getElementById("Atv06_Paragrafo").innerHTML = Atv06_Paragrafo
+        document.getElementById("Atv06_Img").src = Atv06_Img
+        document.getElementById("Atv06_A").href = Atv06_A
+    
+        //Atv07
+        document.getElementById("Ano").innerHTML = Ano
+        document.getElementById("Atv07_Titulo").innerHTML = Atv07_Titulo
+        document.getElementById("Atv07_Paragrafo").innerHTML = Atv07_Paragrafo
+        document.getElementById("Atv07_Img").src = Atv07_Img
+        document.getElementById("Atv07_A").href = Atv07_A
+    
+        //Atv08
+        document.getElementById("Ano").innerHTML = Ano
+        document.getElementById("Atv08_Titulo").innerHTML = Atv08_Titulo
+        document.getElementById("Atv08_Paragrafo").innerHTML = Atv08_Paragrafo
+        document.getElementById("Atv08_Img").src = Atv08_Img
+        document.getElementById("Atv08_A").href = Atv08_A
+    
+        //Atv09
+        document.getElementById("Ano").innerHTML = Ano
+        document.getElementById("Atv09_Titulo").innerHTML = Atv09_Titulo
+        document.getElementById("Atv09_Paragrafo").innerHTML = Atv09_Paragrafo
+        document.getElementById("Atv09_Img").src = Atv09_Img
+        document.getElementById("Atv09_A").href = Atv09_A
+    
+        //Atv10
+        document.getElementById("Ano").innerHTML = Ano
+        document.getElementById("Atv10_Titulo").innerHTML = Atv10_Titulo
+        document.getElementById("Atv10_Paragrafo").innerHTML = Atv10_Paragrafo
+        document.getElementById("Atv10_Img").src = Atv10_Img
+        document.getElementById("Atv10_A").href = Atv10_A
+    
+        //Atv11
+        document.getElementById("Ano").innerHTML = Ano
+        document.getElementById("Atv11_Titulo").innerHTML = Atv11_Titulo
+        document.getElementById("Atv11_Paragrafo").innerHTML = Atv11_Paragrafo
+        document.getElementById("Atv11_Img").src = Atv11_Img
+        document.getElementById("Atv11_A").href = Atv11_A
+    
+        //Atv12
+        document.getElementById("Ano").innerHTML = Ano
+        document.getElementById("Atv12_Titulo").innerHTML = Atv12_Titulo
+        document.getElementById("Atv12_Paragrafo").innerHTML = Atv12_Paragrafo
+        document.getElementById("Atv12_Img").src = Atv12_Img
+        document.getElementById("Atv12_A").href = Atv12_A
+        }
+    
+    
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+    
+
+function aplicar(){
+    alert("foi")
+}
